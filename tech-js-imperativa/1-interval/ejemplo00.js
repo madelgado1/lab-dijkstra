d6528c2ae35d45fb9f56bb32129a2a5854b43cfe
madelgado1 <ma.delgado@laguntel.com>
2023-03-13 20:11:24 +0100

    crear package.json

---
+++ tech-js-imperativa/1-interval/ejemplo00.js
@@ -0,0 +1,9 @@
+const { Console } = require("./console");
+
+const console = new Console();
+console.writeln();
+console.write(`pierDeN o ga`);
+console.writeln(NaN);
+console.writeln(true);
+console.writeln(1);
+console.writeln(Infinity);
